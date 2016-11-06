7f28f2ce1bc3ccd017663ffc7ee084be7d428657
Bida <mohamedchiheb.bida@esprit.tn>
2016-11-06 12:15:12 +0100

    first commit message

---
+++ PiX/PiX.GUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PiX.GUI.MvcApplication" Language="C#" %>
---
+++ PiX/PiX.GUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PiX.GUI.Startup))]
+namespace PiX.GUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
